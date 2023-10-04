065c78c1e5b6fbfcbd7ad995e9424ac9490dac6e
Piti-14 <alupit526@ieselcaminas.org>
2023-10-04 16:09:16 +0200

    added toast and selection in radio buttons. Toast prompt desired message have problems

--- app/src/main/java/com/example/playjuegos/Preferences.kt
+++ app/src/main/java/com/example/playjuegos/Preferences.kt
@@ -1,11 +1,13 @@
 package com.example.playjuegos
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
@@ -15,16 +17,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun MenuPreferences() {
+    var radiusState by rememberSaveable { mutableStateOf("") }
 
     Column (
         modifier = Modifier.fillMaxSize(),
@@ -37,88 +42,92 @@ fun MenuPreferences() {
 
     }
 
-    Box(Modifier.fillMaxSize().padding(25.dp), contentAlignment = Alignment.BottomEnd) {
-        StartFAB()
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(25.dp), contentAlignment = Alignment.BottomEnd) {
+        StartFAB(radiusState, steps)
     }
 }
 
 @Composable
 fun InitButtons() {
+
     Row {
         RadioButton(
-            selected = false,
-            onClick = { /*TODO*/ },
+            selected = radiusState == "Angry Birds",
+            onClick = { radiusState = "Angry Birds"},
             enabled = true,
             colors = RadioButtonDefaults.colors(selectedColor = Color.Yellow, unselectedColor = Color.Black)
         )
 
-        Text(text = "Opción 1", Modifier.padding(top = 12.dp))
+        Text(text = "Angry Birds", Modifier.padding(top = 12.dp))
     }
 
     Row {
         RadioButton(
-            selected = false,
-            onClick = { /*TODO*/ },
+            selected = radiusState == "Dragon Fly",
+            onClick = { radiusState = "Dragon Fly"},
             enabled = true,
             colors = RadioButtonDefaults.colors(selectedColor = Color.Yellow, unselectedColor = Color.Black)
         )
 
-        Text(text = "Opción 2", Modifier.padding(top = 12.dp))
+        Text(text = "Dragon Fly", Modifier.padding(top = 12.dp))
     }
 
     Row {
         RadioButton(
-            selected = false,
-            onClick = { /*TODO*/ },
+            selected = radiusState == "Hill Climbing Racing",
+            onClick = { radiusState = "Hill Climbing Racing"},
             enabled = true,
             colors = RadioButtonDefaults.colors(selectedColor = Color.Yellow, unselectedColor = Color.Black)
         )
 
-        Text(text = "Opción 3", Modifier.padding(top = 12.dp))
+        Text(text = "Hill Climbing Racing", Modifier.padding(top = 12.dp))
     }
 
     Row {
         RadioButton(
-            selected = false,
-            onClick = { /*TODO*/ },
+            selected = radiusState == "Pocket Soccer",
+            onClick = { radiusState = "Pocket Soccer"},
             enabled = true,
             colors = RadioButtonDefaults.colors(selectedColor = Color.Yellow, unselectedColor = Color.Black)
         )
 
-        Text(text = "Option 4", Modifier.padding(top = 12.dp))
+        Text(text = "Pocket Soccer", Modifier.padding(top = 12.dp))
     }
 
     Row {
         RadioButton(
-            selected = false,
-            onClick = { /*TODO*/ },
+            selected = radiusState == "Radiant Defense",
+            onClick = { radiusState = "Radiant Defense"},
             enabled = true,
             colors = RadioButtonDefaults.colors(selectedColor = Color.Yellow, unselectedColor = Color.Black)
         )
 
-        Text(text = "Opción 5", Modifier.padding(top = 12.dp))
+        Text(text = "Radiant Defense", Modifier.padding(top = 12.dp))
     }
 
     Row {
         RadioButton(
-            selected = false,
-            onClick = { /*TODO*/ },
+            selected = radiusState == "Ninja Jump",
+            onClick = {radiusState = "Ninja Jump"},
             enabled = true,
             colors = RadioButtonDefaults.colors(selectedColor = Color.Yellow, unselectedColor = Color.Black)
         )
 
-        Text(text = "Opción 6", Modifier.padding(top = 12.dp))
+        Text(text = "Ninja Jump", Modifier.padding(top = 12.dp))
     }
 
     Row {
         RadioButton(
-            selected = false,
-            onClick = {  },
+            selected = radiusState == "Air Control",
+            onClick = { radiusState = "Air Control"},
             enabled = true,
             colors = RadioButtonDefaults.colors(selectedColor = Color.Yellow, unselectedColor = Color.Black)
         )
 
-        Text(text = "Opción 7", Modifier.padding(top = 12.dp))
+        Text(text = "Air Control", Modifier.padding(top = 12.dp))
     }
 }
 
@@ -135,24 +144,27 @@ fun ShowSlider() {
         steps = steps,
         onValueChange = {selection = it},
         modifier = Modifier.padding(15.dp,0.dp, 15.dp,0.dp),
-        //colors = SliderDefaults.colors(Color.Gray)
+        //colors = SliderDefaults.colors(Color.Yellow)
     )
 }
 
 @Composable
-fun StartFAB() {
+fun StartFAB(radiusState: String, sliderRange: Double) {
     val context = LocalContext.current
     var message = ""
 
     FloatingActionButton(
         onClick = {
-            if () {
-                message = "Has seleccionado gameOption con una puntuación de rangoSlider"
+            if (radiusState == "") {
+                message = "Has seleccionado $radiusState con una puntuación de $sliderRange"
             } else {
                 message = "No has seleccionado ninguna aplicación"
             }
 
             Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-        }
-    ){}
+        },
+        shape = CircleShape,
+    ){
+        Image(painter = painterResource(id = R.drawable.tick), contentDescription = "Tick")
+    }
 }
